4efd8e977b82967f3f5b1988f9bc118b455971c6
kee <seankee8@gmail.com>
2025-09-25 14:57:08 -0500

    Random functions added

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 def say_hello():
     print("Hello, World!")
 
+def broadcast_hello_to_entire_class():
+    for student in range(1, 31):
+        print(f"Hello, Student {student}!")
+
 if __name__ == "__main__":
     say_hello()
+    broadcast_hello_to_entire_class()
