bbbfa774b6b41e710fd7a65d83cf1daae4f5df89
kee <seankee8@gmail.com>
2025-09-25 15:01:04 -0500

    Delete random functions

--- hello.py
+++ hello.py
@@ -5,15 +5,6 @@ def broadcast_hello_to_entire_class():
     for student in range(1, 31):
         print(f"Hello, Student {student}!")
 
-def random_function():
-    pass
-
-def random_function_two():
-    pass
-
-def random_function_three():
-    pass
-
 
 if __name__ == "__main__":
     say_hello()
